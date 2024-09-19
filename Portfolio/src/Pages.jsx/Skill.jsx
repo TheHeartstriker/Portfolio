9e5d5e38349192dde89f13927f0c9715201d6c39
Kaden <kadenw258@gmail.com>
2024-09-19 07:29:58 -0500

    About page
    Did some work for the about page along with organizing some of the files

---
+++ Portfolio/src/Pages.jsx/Skill.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function Skill() {
+  return (
+    <>
+      <h1 id="SkillHeader">Skills</h1>
+      <div className="SkillContainer">
+        <h2>HTML, CSS, Javascript</h2>
+      </div>
+      <div className="Project1">
+        <h2>Project 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+          veniam officiis voluptate, harum id molestiae distinctio similique
+          quisquam! Natus consequatur quos consequuntur quam doloribus vero ea.
+          Eum cum corrupti reprehenderit!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Skill;
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Components
-import About from "./About.jsx";
-import Skill from "./Skill.jsx";
-import Contact from "./Contact.jsx";
+import About from "./Pages.jsx/About.jsx";
+import Skill from "./Pages.jsx/Skill.jsx";
+import Contact from "./Pages.jsx/Contact.jsx";
 //Nav components
 import Nav from "./Nav.jsx";
 //Styles
 import "./Navigate.css";
-import "./index.css";
+import "./PagesCss/about.css";
+import "./PagesCss/skill.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
