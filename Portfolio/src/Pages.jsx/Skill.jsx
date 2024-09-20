6c94ea9fd693e3f0014ba8fe850fe8f798577e28
Kaden <kadenw258@gmail.com>
2024-09-20 07:14:27 -0500

    Project set up
    Did the baseline for the project setup adding random text and containers for it

--- Portfolio/src/Pages.jsx/Skill.jsx
+++ Portfolio/src/Pages.jsx/Skill.jsx
@@ -8,13 +8,35 @@ function Skill() {
         <h2>HTML, CSS, Javascript</h2>
       </div>
       <div className="Project1">
-        <h2>Project 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
-          veniam officiis voluptate, harum id molestiae distinctio similique
-          quisquam! Natus consequatur quos consequuntur quam doloribus vero ea.
-          Eum cum corrupti reprehenderit!
-        </p>
+        <h2>Dynamic animations</h2>
+        <div className="Project1Text">
+          <h4>
+            A front end project that uses React and HTML canvas to create
+            interactive and controlable dynamic animations.
+          </h4>
+          <h3>Link</h3>
+          <h3>Code</h3>
+        </div>
+      </div>
+      {/* Second Project */}
+      <div className="Project2">
+        <h2>Responsive Design</h2>
+        <div className="Project2Text">
+          <h4>
+            A front end project that uses React and CSS to create a responsive
+            design that works on all devices.
+          </h4>
+        </div>
+      </div>
+      {/* Third Project */}
+      <div className="Project3">
+        <h2>Full Stack Development</h2>
+        <div className="Project3Text">
+          <h4>
+            A full stack project that uses React, Node.js, and MongoDB to create
+            a full stack web application.
+          </h4>
+        </div>
       </div>
     </>
   );
