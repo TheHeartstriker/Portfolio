24bf3145fb91b7cdd3432cb658f7423b73d8fc36
Kaden <kadenw258@gmail.com>
2025-01-24 13:11:36 -0600

    File organization
    Re-organized the files

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Components
-import About from "./Pages.jsx/About.jsx";
-import Skill from "./Pages.jsx/Skill.jsx";
-import Contact from "./Pages.jsx/Contact.jsx";
-import Background from "./Pages.jsx/Backgrond.jsx";
+import About from "./About/About.jsx";
+import Skill from "./Skills/Skill.jsx";
+import Contact from "./Contact/Contact.jsx";
+import Background from "./Backgrounds/Backgrond.jsx";
 //Nav components
 import Nav from "./Nav.jsx";
 //Styles
 import "./Navigate.css";
-import "./PagesCss/about.css";
-import "./PagesCss/skill.css";
-import "./PagesCss/contact.css";
-import "./PagesCss/background.css";
+import "./About/about.css";
+import "./Skills/skill.css";
+import "./Contact/contact.css";
+import "./Backgrounds/background.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
