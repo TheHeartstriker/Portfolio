fe5b7a5235c06c89bd4a78bc64f38082309183a7
Kaden <kadenw258@gmail.com>
2025-05-18 11:39:31 -0500

    More logistics
    I did more logistics here. You can now display and see a article. It also looks nice next is to setup markdown.

--- Portfolio/src/components/scriptorium/index.jsx
+++ Portfolio/src/components/scriptorium/index.jsx
@@ -1,12 +1,29 @@
-export function SubjectContainer({ title, subject, description }) {
+export function SubjectContainer({
+  title,
+  subject,
+  description,
+  active,
+  onClick,
+}) {
   return (
-    <div className="subject-container">
+    <div className="subject-container" onClick={onClick}>
       <div className="subjectTextContainer">
         <h1>{title}</h1>
         <h2>{subject}</h2>
       </div>
       <div className="subjectDescriptionContainer">
         <p>{description}</p>
+      </div>
+      <div className="mainArticle">
+        <div className={`articleTextContainer${active ? " active" : ""}`}>
+          <h1>Article</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt, nunc at bibendum facilisis, nunc nisl aliquet nunc, eget
+            aliquam nisl nunc eget nunc. Sed tincidunt, nunc at bibendum
+            facilisis, nunc nisl aliquet nunc, eget aliquam nisl nunc eget nunc.
+          </p>
+        </div>
       </div>
     </div>
   );
--- Portfolio/src/pages/scriptorium/scriptorium.jsx
+++ Portfolio/src/pages/scriptorium/scriptorium.jsx
@@ -4,12 +4,25 @@ import { SubjectContainer } from "../../components/scriptorium/index.jsx";
 import "./scriptorium.css";
 
 function Scriptorium() {
+  const [activeArts, setActiveArts] = useState({
+    article1: false,
+  });
+
+  function handleClick(article) {
+    setActiveArts((prevState) => ({
+      ...prevState,
+      [article]: !prevState[article],
+    }));
+  }
+
   return (
     <div className="mainScriptoriumContainer">
       <SubjectContainer
         title="Particle systems"
         subject="Mathematics's / programming "
         description="An introduction to creative coding. Overviews a base line for creating fun and unique web experiences. Such techniques were even used to create this website! Also something great to learn to practice fundamentals and improve problem solving skills. "
+        active={activeArts.article1}
+        onClick={() => handleClick("article1")}
       />
     </div>
   );
