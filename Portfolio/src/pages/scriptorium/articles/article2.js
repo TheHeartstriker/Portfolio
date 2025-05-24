08b25f77672f3a0cd6ab49239253553775e93d68
Kaden <kadenw258@gmail.com>
2025-05-24 15:10:14 -0500

    Setup article2
    Setup the next article. The rest should just be writing and img's.

---
+++ Portfolio/src/pages/scriptorium/articles/article2.js
@@ -0,0 +1,7 @@
+export const articleMappingFullStack = `
+# Mapping a Full Stack Application
+This article is going to go over system design its a high level overview going over what I think are the most important steps in the process of using a fullstack application. There is going to three sections, first is serving code
+this will go over what happens when you visit a fullstack application, the second is logging in this is a smaller section but in most fullstack applications being logged is fundamental the third section is common opperations
+this section is going to go over what happens when someone logs in and is using the application, where does jwt fit? Cookies? Cors?
+
+`;
--- Portfolio/src/pages/scriptorium/scriptorium.jsx
+++ Portfolio/src/pages/scriptorium/scriptorium.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { SubjectContainer } from "../../components/scriptorium/index.jsx";
 import { markdown } from "./articles/article1.js";
+import { articleMappingFullStack } from "./articles/article2.js";
 import particleSys from "./scripts/particleSys.jsx";
 
 import "./scriptorium.css";
 
 function Scriptorium() {
-  const articleArr = [markdown, particleSys];
+  const articleArr1 = [markdown, particleSys];
+  const articleArr2 = [articleMappingFullStack];
   const [activeArts, setActiveArts] = useState({
     article1: false,
+    article2: false,
   });
 
   function handleClick(article, furl) {
@@ -27,8 +30,18 @@ function Scriptorium() {
         description="An introduction to creative coding through a particle system. Learn how logic, math, and code can recreate natural phenomena. These techniques power parts of this website and are an invaluable way to practice fundamentals and improve problem-solving skills."
         active={activeArts.article1}
         onClick={handleClick}
-        article={articleArr}
+        article={articleArr1}
         articleName="article1"
+      />
+
+      <SubjectContainer
+        title="Maping a full stack application"
+        subject="Web development / system design"
+        description="A maping of a full stack application, using custom drawn diagrams an images to showcase the architecture and flow of a full stack application removed from abstractions like saas. This is a very fundamentall article its about what happens when you visit a website, serving code, domain names, logging in, jwt and cookies."
+        active={activeArts.article2}
+        onClick={handleClick}
+        article={articleArr2}
+        articleName="article2"
       />
     </div>
   );
