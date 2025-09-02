1129ed5658d7c78c470382a6eaae2d24f4c6c346
Kaden <kadenw258@gmail.com>
2025-09-02 03:19:10 -0500

    Text change

--- Portfolio/src/views/about/text.js
+++ Portfolio/src/views/about/text.js
@@ -1,9 +1,11 @@
 export const Header1 = "Who am I?";
 export const MainText1 = `Hi, I'm Kaden Wildauer, a software engineer specializing in fullstack development. I am based in the US and have spent thousands of hours honing my craft. I have
-built a wide variety of projects, most personal with some freelance mixed in. You can view many of them here!`;
-export const Header2 = "My work";
-export const MainText2 = `My work is rooted in building things that are functional, engaging, and well-crafted.
-Whether that’s writing clean and intuitive code, designing text blurbs that can be moved around, or shaping user experiences and interfaces, I enjoy creating things that are polished, enjoyable, and easy to use.`;
+built a wide variety of projects, most personal with some freelance mixed in. You can view most of my work and creations here!`;
+export const Header2 = "My approach";
+export const MainText2 = `My approach to programming and development is a mix of stubbornness and creativity. I like to push the limits of what I can do
+and what I can create . I am always looking for ways to better my creations. Constantly learning and improving, updating, iterating. That lead
+me down a few interesting paths like learning C++ and Web Assembly actually designing things in figma. As a programmer I am creative, stubborn and
+a bit of a perfectionist.`;
 export const Header3 = "More than just code";
 export const MainText3 = `For me programming is more than just code. It's both art and part of my identity. I want to create things that are not only functional but beautiful.
 Something I can look back on and be proud of. A skill I want to push to its limits or my limits at least. That's what code is to me. A reflection of both creativity and dedication.`;
