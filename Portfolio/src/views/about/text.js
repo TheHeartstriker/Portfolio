fa6d42eab7e6ccee8d1c7216c1c07560b638b280
Kaden <kadenw258@gmail.com>
2025-10-04 12:52:07 -0500

    About me text changes
    Finally changed the text for the about me page. Going to keep it no reason to change.

--- Portfolio/src/views/about/text.js
+++ Portfolio/src/views/about/text.js
@@ -1,11 +1,8 @@
 export const Header1 = "Who am I?";
-export const MainText1 = `Hi, I'm Kaden Wildauer, a software engineer specializing in fullstack development. I am based in the US and have spent thousands of hours honing my craft. I have
-built a wide variety of projects, most personal with some freelance mixed in. You can view most of my work and creations here!`;
-export const Header2 = "My approach";
-export const MainText2 = `My approach to programming and development is a mix of stubbornness and creativity. I like to push the limits of what I can do
-and what I can create . I am always looking for ways to better my creations. Constantly learning and improving, updating, iterating. That lead
-me down a few interesting paths like learning C++ and Web Assembly actually designing things in figma. As a programmer I am creative, stubborn and
-a bit of a perfectionist.`;
-export const Header3 = "More than just code";
-export const MainText3 = `For me programming is more than just code. It's both art and part of my identity. I want to create things that are not only functional but beautiful.
-Something I can look back on and be proud of. A skill I want to push to its limits or my limits at least. That's what code is to me. A reflection of both creativity and dedication.`;
+export const MainText1 = `Hi, I'm Kaden Wildauer, a full stack software engineer, freelancer, and tech writer. I’ve spent thousands of hours honing my craft, building projects, apps, and articles. Most of my work and content can be explored right here!`;
+export const Header2 = "My work";
+export const MainText2 = `I’ve built a lot of things, software, web apps, tech articles, and social posts. 
+I always try to create something original, whether it’s a program worth talking about or an article I wish I had. When you look through my work, that’s what you’ll find: projects and content designed to be useful, creative, and a little different.`;
+export const Header3 = "Beyond code";
+export const MainText3 = `Outside of software engineering, writing, and freelancing, I like to read, draw (I’m terrible), and play piano (also bad), but I love it anyway.
+ I also listen to an almost ridiculous amount of music (about 2,000 hours a year). Beyond that, I enjoy learning new things, meeting new people, playing games and fitness. That’s pretty much me!`;
