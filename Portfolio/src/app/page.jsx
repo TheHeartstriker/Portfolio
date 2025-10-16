a5b43cbd8c86fabb20196b0b687ff566c6ee1474
Kaden <kadenw258@gmail.com>
2025-10-16 15:00:09 -0500

    Update page.jsx

--- Portfolio/src/app/page.jsx
+++ Portfolio/src/app/page.jsx
@@ -1,4 +1,5 @@
 export const metadata = {
+  metadataBase: new URL("https://www.kadenwildauer.com/"),
   title: "Kaden Wildauer | Full-Stack Developer",
   description:
     "Explore my portfolio as a web developer, freelancer, and software engineer. Discover my projects, skills, coding playgrounds, and creative experiments in web development and software engineering.",
@@ -25,6 +26,9 @@ export const metadata = {
     images: ["/mainThumb.webp"],
   },
   robots: "index, follow",
+  alternates: {
+    canonical: "https://www.kadenwildauer.com/", // Added to enforce canonical URL
+  },
 };
 
 import About from "../views/about/about.jsx";
