a5490cc28f3827d6492cb979489b21f5e09e1c1e
Kaden <kadenw258@gmail.com>
2025-10-16 14:13:17 -0500

    Update page.jsx

--- Portfolio/src/app/page.jsx
+++ Portfolio/src/app/page.jsx
@@ -8,7 +8,6 @@ export const metadata = {
     title: "Kaden Wildauer | Full-Stack Developer",
     description:
       "Explore my portfolio as a web developer, freelancer, and software engineer. Discover my projects, skills, coding playgrounds, and creative experiments in web development and software engineering.",
-    url: "https://kadenwildauer.com",
     images: [
       {
         url: "/mainThumb.webp",
