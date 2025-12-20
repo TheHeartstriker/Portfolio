d00e239df6bc5880906319e5533bfad15d3b9e9f
Kaden <kadenw258@gmail.com>
2025-12-20 13:01:51 -0600

    Mostly done footer
    The footer is mostly done the shape or outline is complete along with a good portion of the content. The functionality is incomplete though.

--- Portfolio/src/components/footer/footer.jsx
+++ Portfolio/src/components/footer/footer.jsx
@@ -1,26 +1,53 @@
 import "./footer.css";
+import LogoLinkedIn from "/public/logo-linkedin";
+import TwitterLogo from "/public/logo-twitter";
+import UpworkRoundedsquare from "/public/upwork-roundedsquare";
+import GitHubLogo from "/public/icons/Github";
 function Footer() {
   const navPages = ["About me", "Skills", "scriptorium", "Contact"];
   const socials = ["GitHub", "LinkedIn", "Twitter", "Upwork"];
 
   return (
     <>
-      <div className="container-for-corners"></div>
+      <div className="container-for-corners">
+        <span className="corner-square "></span>
+        <span className="corner-square "></span>
+      </div>
       <footer className="footer-container">
         <div className="footer-content-container"></div>
-        <div className="footer-content-container"></div>
+        {/* Theme container */}
+        <div className="footer-content-container">
+          <span className="footer-theme-element">
+            <h5>Autumn</h5>
+          </span>
+          <span className="footer-theme-element">
+            <h5>Spring</h5>
+          </span>
+          <span className="footer-theme-element">
+            <h5>Summer</h5>
+          </span>
+          <span className="footer-theme-element">
+            <h5>Winter</h5>
+          </span>
+        </div>
         {/* Pill container's */}
+        {/* Socials */}
         <div className="footer-content-container">
           {socials.map((social, index) => (
             <div key={index} className="blue-pill">
-              <h4>{social}</h4>
+              <h5>{social}</h5>
+              {social === "GitHub" && <GitHubLogo />}
+              {social === "LinkedIn" && <LogoLinkedIn />}
+              {social === "Twitter" && <TwitterLogo />}
+              {social === "Upwork" && <UpworkRoundedsquare />}
             </div>
           ))}
         </div>
+        {/* Navigation Pages */}
         <div className="footer-content-container">
           {navPages.map((page, index) => (
             <div key={index} className="blue-pill">
-              <h4>{page}</h4>
+              <h5>{page}</h5>
             </div>
           ))}
         </div>
