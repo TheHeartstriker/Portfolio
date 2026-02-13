22d7bbad489ae1252625ba0d69227c5ae620507a
Kaden <kadenw258@gmail.com>
2026-02-13 13:39:11 -0600

    Color switch
    Edited to new color's the main accent is more visable also removed all glass.

--- Portfolio/src/components/forStyle/themeGen/themes.js
+++ Portfolio/src/components/forStyle/themeGen/themes.js
@@ -47,25 +47,25 @@ export const autumnTheme = {
 
 // export const professionalTheme1 = {
 //   // The 30%
-//   "color-1": "#7A4F35",
+//   "color-1": "#9C6644",
 //   //The 10%
-//   "color-2": "#635147",
+//   "color-2": "#6E5B4B",
 //   // The 60%
-//   "color-3": "#F4EDD7",
-//   "color-4": "#EEE4C4",
-//   "color-5": "#E8DBB0",
+//   "color-3": "#F5EED6",
+//   "color-4": "#EFE5C3",
+//   "color-5": "#EADCAE",
 
 //   dark: false,
 // };
 
 export const professionalTheme1 = {
   // The 30%
-  "color-1": "#9C6644",
+  "color-1": "#83563A",
   //The 10%
   "color-2": "#6E5B4B",
   // The 60%
-  "color-3": "#F5EED6",
-  "color-4": "#EFE5C3",
+  "color-3": "#F7F1DE",
+  "color-4": "#F0E6C7",
   "color-5": "#EADCAE",
 
   dark: false,
